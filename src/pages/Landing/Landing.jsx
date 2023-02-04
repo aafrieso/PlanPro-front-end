221f2ac72c002cc8e1eb41f21a119cf57912b45f
Jeremy <jrob1time@gmail.com>
2023-02-04 10:39:03 -0600

    update margin for landing page logo

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -11,37 +11,17 @@ const Landing = () => {
         <section className={styles.about}>
           <header>
             <h1>ABOUT US</h1>
-            <h3>HOO WE ARE</h3>
+            <h3>Why Productivity?</h3>
           </header>
           <article>
             <p>
-              Not everyone is a morning person. That's why we're building Hoot, an open and inclusive place for night owls to share their ideas, thoughts, and knowledge with one another. We provide a platform for people to share ideas in the early hours of the morning when owl brains work best.
-              Hoot is a community of self-identifying owls like you, that wants to make sure you always have something interesting to say no matter what time of day it is. You no longer have to worry about your troubles keeping you up during the day. You can now blog about your favorite topics and connect with other owls at night. With Hoot, it's never too late to post.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sodales felis in justo suscipit, a ornare nibh euismod. Nunc laoreet dapibus nunc, eu malesuada nunc mollis ut. Donec at justo at massa vulputate suscipit. Proin auctor eget neque non ultrices. Quisque euismod tellus eu dui ultrices, eu efficitur ex fringilla. Quisque eget ex a elit venenatis dapibus gravida ac sem. Nunc quis felis sit amet est varius ullamcorper. Integer viverra libero sit amet consectetur viverra.
             </p>
-          </article>
-        </section>
-
-        <section className={styles.testimonial}>
-          <header>
-            <h3>WHO GIVES A HOOT?</h3>
-            <h1>TESTIMONIALS</h1>
-          </header>
-          <article>
-            <header>
-              <h4>Ben Manley</h4>
-              <p>Software Engineer</p>
-            </header>
-            <p>
-              I found Hoot through a friend of mine, and I'm so glad I did. As a night owl, I have a hard time finding blogging apps that fit my lifestyle. The interface is so easy to use and makes it really convenient for me to write my blogs at night.
-            </p>
-            <footer>
-              <img src={Logotype} alt="logo" />
-            </footer>
           </article>
         </section>
       </main>
       <footer className={styles.footer}>
-        <p>© 2022 HOOT INC. OWL RIGHTS RESERVED</p>
+        <p>© 2022 PLANPRO INC. ALL RIGHTS RESERVED</p>
       </footer>
     </>
   )
