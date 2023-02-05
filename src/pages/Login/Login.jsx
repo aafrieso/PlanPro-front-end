2f0175918fab68e18d7527b6d0fa8a1d3701277a
Jeremy <jrob1time@gmail.com>
2023-02-05 10:50:02 -0600

    update login styling

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import LoginForm from '../../components/LoginForm/LoginForm'
 import styles from './Login.module.css'
-import LoginIcon from '../../assets/branding/PlanProAll.png'
 
 const LoginPage = props => {
   const [message, setMessage] = useState([''])
@@ -11,13 +10,10 @@ const LoginPage = props => {
   }
 
   return (
+    
     <main className={styles.container}>
       <section>
-        <img src={LoginIcon} alt="logo" />
-      </section>
       <h1>Log In</h1>
-      <p>{message}</p>
-      <section>
         <LoginForm
           message={message}
           updateMessage={updateMessage}
