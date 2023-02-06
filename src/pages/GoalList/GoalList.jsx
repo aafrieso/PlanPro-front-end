3429b813cc3a10865c24bcbec2aa2c2c52cdd8d7
Austin Friesorger <aafrieso@svsu.edu>
2023-02-06 13:26:14 -0500

    move folders for goals

---
+++ src/pages/GoalList/GoalList.jsx
@@ -0,0 +1,11 @@
+import styles from './GoalList.module.css'
+
+const GoalList = (props) => {
+  return (
+    <main className={styles.container}>
+      Goal List
+    </main>
+  )
+}
+
+export default GoalList
