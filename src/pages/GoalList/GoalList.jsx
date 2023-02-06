63104a90f14a35e2a01511398fa5de69f5d6b3ab
Austin Friesorger <aafrieso@svsu.edu>
2023-02-06 14:07:07 -0500

    update goal list component

--- src/pages/GoalList/GoalList.jsx
+++ src/pages/GoalList/GoalList.jsx
@@ -1,12 +1,11 @@
 import styles from './GoalList.module.css'
+import GoalCard from "../../components/GoalCard/GoalCard"
 
 const GoalList = (props) => {
   return (
     <main className={styles.container}>
       {props.goals.map((goal) => (
-        <p key={goal._id}>
-          {goal.title}
-        </p>
+        <GoalCard key={goal._id} goal={goal} />
       ))}
     </main>
   )
