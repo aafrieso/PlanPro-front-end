f5f4a8a91a0dbd7f7af0ab9d164c47979372f199
Megan Smith <megans766@gmail.com>
2023-02-06 20:40:12 -0500

    update GoalDetail component

--- src/components/Goal/GoalDetail.jsx
+++ src/components/Goal/GoalDetail.jsx
@@ -1,13 +1,26 @@
 import MyProfile from "../../pages/MyProfile/MyProfile"
 import { Link } from "react-router-dom"
 import { useState, useEffect } from "react"
+import * as goalService from '../../services/goalService'
+import { useParams } from "react-router-dom"
 
-const Goal = () => {
+const GoalDetail = () => {
+  const { id } = useParams()
+  const [goal, setGoal] = useState(null)
+
+  useEffect(() => {
+    const fetchGoal = async () => {
+      const goalData = await goalService.getGoal(id)
+      setGoal(goalData)
+    }
+    fetchGoal()
+  }, [id])
+
   return (
     <>
-    
+
     </>
   )
 }
 
-export default Goal
+export default GoalDetail
