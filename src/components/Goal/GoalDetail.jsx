6085de645d227addfc3a9dcb8a0dd01eb60de0e4
Megan Smith <megans766@gmail.com>
2023-02-06 20:22:52 -0500

    create GoalDetail component

---
+++ src/components/Goal/GoalDetail.jsx
@@ -0,0 +1,13 @@
+import MyProfile from "../../pages/MyProfile/MyProfile"
+import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+
+const Goal = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Goal
