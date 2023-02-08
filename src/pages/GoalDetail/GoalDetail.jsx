66516c1655c4076a20f3f72c3036ac3404e199fe
Megan Smith <megans766@gmail.com>
2023-02-07 21:23:15 -0500

    create NewTask form

--- src/pages/GoalDetail/GoalDetail.jsx
+++ src/pages/GoalDetail/GoalDetail.jsx
@@ -2,11 +2,19 @@ import { useState, useEffect } from "react"
 import * as goalService from "../../services/goalService"
 import { useParams } from "react-router-dom"
 import TaskList from "../../components/TaskList/TaskList"
+import NewTask from "../../components/NewTask/NewTask"
+import * as taskService from "../../services/taskService"
 
 const GoalDetail = () => {
   const { goalId } = useParams()
   console.log("THIS IS A GOALID", goalId);
   const [goal, setGoal] = useState(null)
+
+  const handleAddTask = async (formData) => {
+    formData.goalListId = goalId
+    const data = await taskService.createTask(formData)
+    setGoal(data)
+  }
 
   useEffect(() => {
     const fetchGoal = async () => {
@@ -22,6 +30,7 @@ const GoalDetail = () => {
   return (
     <main>
       <h1>{goal.title}</h1>
+      <NewTask handleAddTask={handleAddTask} />
       <TaskList tasks={goal.tasks}/>
     </main>
   )
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -2,6 +2,22 @@ import * as tokenService from './tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/tasks`
 
+const createTask = async (data) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 
 const createStep = async (taskId, stepData) => {
   try {
@@ -21,4 +37,5 @@ const createStep = async (taskId, stepData) => {
 
 export { 
   createStep,
+  createTask,
 }
