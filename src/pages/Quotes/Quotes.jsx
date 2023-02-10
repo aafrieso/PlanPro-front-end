fef75816f6d7326756fecc70e74b82821baa337d
Jeremy <jrob1time@gmail.com>
2023-02-10 11:23:00 -0600

    delet bugs

--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
+import styles from './Quotes.module.css'
 
 const Quote = (props) => {
   const [quote, setQuote] = useState({ quote: "" });
-  console.log(quote);
   const handleChange = ({ target }) => {
     setQuote({ ...quote, [target.name]: target.value });
   };
@@ -14,6 +14,8 @@ const Quote = (props) => {
 
   return (
     <>
+      <main className={styles.container}>
+
       <form onSubmit={handleSubmit}>
         <h1>Quote</h1>
         <label> Add an Inspirational Quote here!</label>
@@ -22,22 +24,23 @@ const Quote = (props) => {
           name="quote"
           value={quote.quote}
           onChange={handleChange}
-        />
+          />
         <button style={{ color: "black" }} type="submit">
           Submit
         </button>
       </form>
       {props.quotes.map((quote) => (
         <li key={quote._id}>
-          {quote.quote}
           <button
             style={{ color: "black" }}
             onClick={() => props.handleDeleteQuote(quote._id)}
-          >
+            >
             X
           </button>
+          {quote.quote}
         </li>
       ))}
+      </main>
     </>
   );
 };
